5c8d73947d300d356bcc87f768323afa9346ab66
luelher <luelher@30c56c62-5008-fad1-31ab-0739dbf886b4>
2010-04-27 12:50:15 +0000

    import incial

---
+++ trunk/apuestas/apps/backend/modules/quiniela/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/quinielaGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/quinielaGeneratorHelper.class.php';
+
+/**
+ * quiniela actions.
+ *
+ * @package    sf_sandbox
+ * @subpackage quiniela
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class quinielaActions extends autoQuinielaActions
+{
+}
