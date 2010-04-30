83313bb2aaebc7a409fcc3bb2cc4abc978767834
luelher <luelher@30c56c62-5008-fad1-31ab-0739dbf886b4>
2010-04-30 05:01:31 +0000

    Cambios Varios

--- trunk/apuestas/apps/backend/modules/pagojugador/templates/indexSuccess.php
+++
@@ -1,9 +0,0 @@
-<?php use_stylesheet('/sfDoctrinePlugin/css/global.css', 'first') ?>
-<?php use_stylesheet('/sfDoctrinePlugin/css/default.css', 'first') ?>
-<div id="sf_admin_container">
-<h1>Paso 1: Seleccione las quinielas a cancelar</h1>
-
-<?php echo form_tag_for($form, 'pagojugador') ?>
-  <?php include_partial('list_quinielas', array('form' => $form, 'quinielas' => $quinielas)) ?>
-</form>
-</div>
